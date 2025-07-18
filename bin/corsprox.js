afdefc82e1d38088dcde4ba8155c82a826e700e0
Kiril Strax <kiril@getsensibill.com>
2025-07-18 09:50:08 -0400

    Basic implementation

--- bin/corsprox.js
+++ bin/corsprox.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 
-var proxy = require('../lib/index.js');
-proxy.start()
+import { start } from '../lib/index.js';
+
+// Proxy port to listen on
+const PORT = 3001;
+
+// Proxy path
+const PATH = 'proxy';
+
+// Target API base URL
+const TARGET_API_BASE = 'https://example.com/api';
+
+// Access-Control-Allow-Origin value 
+const ALLOW_ORIGIN = '*';
+
+start(PORT, PATH, TARGET_API_BASE, ALLOW_ORIGIN)
